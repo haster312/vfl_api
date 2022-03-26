7a085079fc8d9a18b588ef2428bdb856136fe9bd
William Nguyen <William.Nguyen.extern@galeria.de>
2022-03-27 02:10:53 +0700

    Add unit test

--- tests/controllers/UserController.spec.ts
+++ tests/controllers/UserController.spec.ts
@@ -1,16 +1,23 @@
 import { Api } from '../helpers/Api'
 import { Connection } from "../../src/database/Connection";
 
-describe('User controller test', () => {
+describe('User Controller Test', () => {
     beforeAll(async () => {
        await Connection.initialize();
     });
 
-    it('Should return a list of users', async () => {
+    it('Get Users, Should return success with list of users', async () => {
         const response = await Api.get('/api/user');
 
         expect(response.statusCode).toEqual(200);
         expect(Array.isArray(response.body.data)).toBe(true);
+    });
+
+    it('Get user by Id, Should return user', async () => {
+        const response = await Api.get('/api/user/1');
+
+        expect(response.statusCode).toEqual(200);
+        // expect(Array.isArray(response.body.data)).toBe(true);
     });
 
     afterAll(async () => {
